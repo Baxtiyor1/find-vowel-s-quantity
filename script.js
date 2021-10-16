4e8de05d45281e08ad726e002d59cf8c0a630937
BaxtiyorXusanov <xusanovbaxtiyor07>
2021-10-16 10:05:53 +0500

    add vowels array

--- script.js
+++ script.js
@@ -2,23 +2,26 @@ const elForm = document.querySelector('#form');
 const elInput = document.querySelector('#input');
 const elResultText = document.querySelector('.result');
 
+let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
 
 elForm.addEventListener('submit', (e) =>{
     e.preventDefault()
-
+    
     elResultText.classList.add('result__active')
-
+    
     let inputValue = elInput.value.trim()
     let resultArr = inputValue.split('')
     let result = 0
     
     resultArr.forEach(item =>{
-        if(item === 'a' || item === 'e' || item === 'i' || item === 'o' || item === 'u'){
+        for(let i = 0; i <= vowels.length; i++)
+        if(item === vowels[i]){
             result++
         }
     })
-
+    
     elResultText.textContent = result
-
+    
     elInput.value = ''
 })
